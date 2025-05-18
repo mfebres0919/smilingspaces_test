f0b9753347e86ec8975b2ac32f2f0c2ae2fabf58
Miguelle Febres <guellefeb21@gmail.com>
2025-05-18 15:55:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const mobileNav = document.getElementById('mobileNav');
+
+hamburger.addEventListener('click', () => {
+  mobileNav.classList.toggle('show');
+});
